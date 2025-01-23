37939e8db78d488c75461d181dcf499782afb51b
verdugong <sebastianvccv@gmail.com>
2025-01-23 15:55:48 -0500

    SistemaMedico V0.0.1

---
+++ Sistema_Medico_VerdugoS_EspinozaJ/src/main/java/ec/edu/ups/bd/sistemamedico/vista/principal/Sistema_Medico_VerdugoS_EspinozaJ.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.ups.bd.sistemamedico.vista.principal;
+
+/**
+ *
+ * @author sebas
+ */
+public class Sistema_Medico_VerdugoS_EspinozaJ {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
