7cdfafc32d56d4a6a68b64dd87520a80baca6849
verdugong <sebastianvccv@gmail.com>
2025-01-29 15:13:43 -0500

    SistemaMedico V0.0.2

---
+++ Sistema_Medico_VerdugoS_EspinozaJ/src/main/java/ec/edu/ups/bd/sistemamedico/dao/UsuarioDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.ups.bd.sistemamedico.dao;
+
+import ec.edu.ups.bd.sistemamedico.idao.*;
+
+/**
+ *
+ * @author sebas
+ */
+public class UsuarioDAO {
+    
+}
